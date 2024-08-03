c9096d9cd481ffdd4c7af9b55fbd04b19158e373
Ridam1999 <35134202+Ridam1999@users.noreply.github.com>
2024-08-03 08:58:57 +0530

    Add files via upload

---
+++ EmployeeServices.cs
@@ -0,0 +1,25 @@
+﻿using learning.Interface;
+using learning.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace learning.Services
+{
+
+    public class EmployeeServices :IEmployee
+    {
+        private readonly EmployeeContext _dbContext;
+        public EmployeeServices(EmployeeContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
+        {
+            
+            var employees = await _dbContext.Employees.ToListAsync();
+            //var employees = await (from e in _dbContext.Employees
+            //                       select e).ToListAsync();
+            return employees;
+        }
+    }
+}
---
+++ IEmployee.cs
@@ -0,0 +1,9 @@
+﻿using learning.Models;
+
+namespace learning.Interface
+{
+    public interface IEmployee
+    {
+        public Task<IEnumerable<Employee>> GetAllEmployees();
+    }
+}
